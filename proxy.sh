57128b2f650a4af3b1beb9c076d7ac7bac7b4d9a
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-01-24 07:03:28 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -3,8 +3,8 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs screen git build-essential
 sudo npm install -g yarn
 sudo yarn global add gulp
+sudo npm install pm2@latest -g
 git clone https://github.com/skypool-org/skypool-nimiq-miner
 cd skypool-nimiq-miner/proxy && sudo yarn
-(crontab -l 2>/dev/null || true; echo "@reboot  cd skypool-nimiq-miner/proxy && screen -d -m node proxy.js") | crontab -
-screen -d -m node proxy.js
+pm2 start proxy.js
 
