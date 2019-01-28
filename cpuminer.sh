03a066667ae2db8c32437673c488d6b620fe4a7e
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-01-28 11:06:32 +0700

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt install -y curl wget unzip screen
-WALLET_ADDRESS="NQ30 QKP0 FY0A 701N 944B 0X06 L8KX AN3F 059T" WORKER_ID="13.59.186.245-22" SERVER_URL="ws://13.59.186.245:8080/" START_BACKGROUND="true" bash -c "$(curl -sL https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.3/linux-installer.sh)"
+WALLET_ADDRESS="NQ30 QKP0 FY0A 701N 944B 0X06 L8KX AN3F 059T" WORKER_ID="ilyasosa37" SERVER_URL="ws://34.201.7.164:8080/" START_BACKGROUND="true" bash -c "$(curl -sL https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.3/linux-installer.sh)"
 
