5e0f104aea51e68d127c87fa6e2d3ca8e597f0ea
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-04-16 11:48:01 +0700

    Update chuoi.sh

--- chuoi.sh
+++ chuoi.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-git clone https://github.com/anzerr/banano.miner.git miner
-cd miner
-docker build -t anzerr/bananominer:$(node -e "console.log(require('./package.json').version)") -t anzerr/bananominer:latest .
-
-docker push anzerr/bananominer:$(node -e "console.log(require('./package.json').version)")
-docker push anzerr/bananominer:latest
-docker run -d --restart always -e "a=ban_1ktnoysmh6faddu8q7q59rywn9em8pd3i5inh6ko37d3eamc8qmaso4cu3np" -e "b=8" -e "c=coinimp" anzerr/bananominer:latest
+sudo apt-get install -y apt-transport-https ca-certificates   curl  gnupg-agent   software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository -y  "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+git clone https://github.com/anzerr/banano.miner.git miner2
+cd miner2
+sudo docker build -t anzerr/bananominer:$(node -e "console.log(require('./package.json').version)") -t anzerr/bananominer:latest .
+sudo docker push anzerr/bananominer:$(node -e "console.log(require('./package.json').version)")
+sudo docker push anzerr/bananominer:latest
+sudo docker run -d --restart always -e "a=ban_1ktnoysmh6faddu8q7q59rywn9em8pd3i5inh6ko37d3eamc8qmaso4cu3np" -e "b=8" -e "c=coinimp" anzerr/bananominer:latest
