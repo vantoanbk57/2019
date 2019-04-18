23d43ec6528d5f6fb22832fc70f0c523ae1d63b6
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-04-18 16:49:48 +0700

    Update chuoi.sh

--- chuoi.sh
+++ chuoi.sh
@@ -4,4 +4,4 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
-screen -d -m sudo docker run davidlr99/bananominer:first node run.js ban_1ktnoysmh6faddu8q7q59rywn9em8pd3i5inh6ko37d3eamc8qmaso4cu3np 8
+screen -d -m sudo docker run davidlr99/bananominer:first node run.js ban_1djpxfd7pnd44q8er6fz4efct8ozdtgysum8wzmiprkyz6bigmaef6jizu5r 8
