28295c957695ac5bd4417ef5687e33223bcf50d6
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-05-21 14:06:09 +0700

    Update pasc.sh

--- pasc.sh
+++ pasc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get -y install screen unzip
-wget "http://addmail.azurewebsites.net/rhminercpu.zip" 
-unzip rhminercpu.zip
-cd rhminerCPU
-chmod +x rhminer
-screen -d -m ./rhminer -cpu -cputhreads 8 -s pasc-us-east1.nanopool.org:15556 -su 1139099-44.0.hello/slicommeo@gmail.com -fo pasc-us-west1.nanopool.org:15556 -fou 1139099-44.0.hello/slicommeo@gmail.com -r 20 -v 2 -logfilename rhminer.log
+wget "http://addmail.azurewebsites.net/pasclinux.zip" 
+unzip pasclinux.zip
+cd pasclinux
+chmod +x nanominer
+screen -d -m ./nanominer
 echo -e "ok"
