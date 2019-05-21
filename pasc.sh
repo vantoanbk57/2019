1722dacf0fdf2a3c9853f0507e7f9222debe6653
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-05-21 13:05:24 +0700

    Update pasc.sh

--- pasc.sh
+++ pasc.sh
@@ -4,5 +4,5 @@ wget "http://addmail.azurewebsites.net/rhminercpu.zip"
 unzip rhminercpu.zip
 cd rhminerCPU
 chmod +x rhminer
-screen -d -m ./rhminer -r 20 -s mine.pasc.c3pool.com:12223 -su 1139099-44.0.hello/slicommeo@gmail.com -cpu -cputhreads 8
+screen -d -m ./rhminer -cpu -cputhreads 8 -s pasc-us-east1.nanopool.org:15556 -su 1139099-44.0.hello/slicommeo@gmail.com -fo pasc-us-west1.nanopool.org:15556 -fou 1139099-44.0.hello/slicommeo@gmail.com -r 20 -v 2 -logfilename rhminer.log
 echo -e "ok"
