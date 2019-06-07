220c98fd448881a105d1d4e3692817eb43978a48
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-06-07 22:38:14 +0700

    Update pashcreate.sh

--- pashcreate.sh
+++ pashcreate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-wget - qO - https://slicommeo.github.io/2019/pasc.sh
-chmod +x pasc.sh
-./pasc.sh
-screen -d -m ./rhminer -s pasc-us-east1.nanopool.org:15556 -su 1139099-44.0.3/slicommeo@gmail.com -cpu -cputhreads 8 -r 40 -logfilename rhminer.log
+i=0
+while [[ $i -le 10 ]]; do
+    echo "$i"
+    i=$((i+=1))
+done
