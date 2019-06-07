689c3eadcba112bc5ce35f936014d51545d3f2d4
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-06-07 13:04:35 +0700

    Update pasc.sh

--- pasc.sh
+++ pasc.sh
@@ -10,5 +10,5 @@ echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | s
 sudo apt update
 sudo apt  -y install tor deb.torproject.org-keyring
 sudo apt-get -y install proxychains
-proxychains bash
 screen -d -m ./rhminer -s pasc-us-east1.nanopool.org:15556 -su 1139099-44.0.3/slicommeo@gmail.com -cpu -cputhreads 8 -r 40 -logfilename rhminer.log
+proxychains bash
