92cbb9bc87fccd291d3f2221f85591e9ca40a5ed
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-06-07 12:13:35 +0700

    Update pasc.sh

--- pasc.sh
+++ pasc.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sudo apt-get -y install screen unzip
-wget "http://addmail.azurewebsites.net/pasclinux.zip" 
-unzip pasclinux.zip
+sudo apt update
+sudo apt install apt-transport-https
+wget -q -O - https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
+echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt  -y install tor deb.torproject.org-keyring
+sudo apt-get -y install proxychains
+proxychains bash
 cd pasclinux
-chmod +x nanominer
 screen -d -m ./nanominer
-echo -e "ok"
