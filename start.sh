82e06e808e8d25114b2fec30f773a89d62937efb
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-06-20 13:31:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-while true
-do
-  echo Mining on mine.pasc.c3pool.com with four CPU thread
-  proxychains ./rhminer -s pasc-us-east1.nanopool.org:15556 -su 1139099-44.0.3/slicommeo@gmail.com -cpu -cputhreads 8 -r 40 -logfilename rhminer.log
-done
+
+# Create a resource group.
+az group create --name myResourceGroup --location westeurope
+
+# Create a new virtual machine, this creates SSH keys if not present.
+az vm create --resource-group myResourceGroup --name myVM --image UbuntuLTS --generate-ssh-keys
