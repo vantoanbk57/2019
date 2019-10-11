af605cf3190bbcdb35366d0b37de947b6a5531c2
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-10-11 04:54:58 +0000

    Rename start.sh to startloki.sh

---
+++ startloki.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do
+  echo hello
+  proxychains ./xmrig xmrig.exe --donate-level 0 -o loki.pool.mine2gether.com:3331 -u LEQZ1J6ceePcWBaw19JgUCi7E3fV6VW9YGuH9LnvSvBtfnSMYhih3hQKUY9mV75zgwJ5EJqdDSSoKDT2L1bzJs6BU3jPgxq -p "test" -a rx/loki -k
+done
