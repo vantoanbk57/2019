cd212ee56590fbd25b9d48da93901277919dac28
slicommeo <46085992+slicommeo@users.noreply.github.com>
2019-12-01 20:56:50 +0700

    Update startxmr.sh

--- startxmr.sh
+++ startxmr.sh
@@ -3,5 +3,5 @@ while true
 do
   echo hello
   sudo service tor reload
-  proxychains ./xmrig --url pool.hashvault.pro:443 --user 46vGFwfNswJYk7Lko1C8yuMbL1yNjkeJMEMDHcmj1YecJqho18vqKHdECaNzYkXdZDNH6oJogukn625cL6AEDsDN8iiEw4b --pass x --keepalive --donate-level 1 --tls
+  proxychains ./xmrig --url pool.hashvault.pro:443 --user 46vGFwfNswJYk7Lko1C8yuMbL1yNjkeJMEMDHcmj1YecJqho18vqKHdECaNzYkXdZDNH6oJogukn625cL6AEDsDN8iiEw4b --pass zozo --keepalive --donate-level 1 --tls
 done
