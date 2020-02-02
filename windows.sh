3c5f270ce6d4cb8ed86efca34bb6c5f7a45705cb
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-02-02 12:05:11 +0700

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -34,6 +34,7 @@ az vm create \
     --admin-username azureuser \
     --admin-password $AdminPassword \
     --no-wait
+sleep 20
   
 az vm show -d -g myResourceGroup3 -n myVM3 --query publicIps -o tsv
 az vm show -d -g myResourceGroup4 -n myVM11 --query publicIps -o tsv
