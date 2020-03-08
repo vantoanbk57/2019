afe64f3dfa56cf1b119878fe8d8014319e4ac7ba
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-03-08 12:18:27 +0700

    Update startxmr.sh

--- startxmr.sh
+++ startxmr.sh
@@ -3,5 +3,5 @@ while true
 do
   echo hello
   sudo service tor reload
-  proxychains ./xmrig --url pool.hashvault.pro:443 --user 4Au1NdJnMCQ2qcsrVwcBLoEnUqiNzRJ5iEyr13VsWbNKAGPzyizyaitiGfw1DR13tfQ93dqFZZs9YCCzrGpwRXVhGmwoHoS --pass $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1) --keepalive --donate-level 1 --tls -t 8
+  proxychains ./xmrig --url pool.hashvault.pro:443 --user 45R8Va7ZsiYhLvTGfUUYKdLTJk7igTR9qTH8YBxqtpPQWzxMijpmwMSYrnQjdpRJgyMH9AS9f7kUmEsmspiYzJVzGXtwSb2 --pass $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1) --keepalive --donate-level 1 --tls -t 8
 done
