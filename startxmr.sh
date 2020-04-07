53caa95b9b8954db4ca72bf3047f723d4330799f
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-04-07 15:26:00 +0700

    Update startxmr.sh

--- startxmr.sh
+++ startxmr.sh
@@ -2,5 +2,6 @@
 while true
 do
   echo hello
-   ./xmrig --url pool.supportxmr.com:443 --user 82h89Q5ezkYJBz1jKfYrb6VJvBh5PqWXs6bEetoskA4sKNkBCFToTUiJzGbsN7FZGTFGGikiDJynoCAyahJq4N6F39JAp4D --pass $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1) --keepalive --donate-level 1 --tls -t 8
+  sudo service tor reload
+  proxychains ./xmrig --url pool.supportxmr.com:443 --user 82h89Q5ezkYJBz1jKfYrb6VJvBh5PqWXs6bEetoskA4sKNkBCFToTUiJzGbsN7FZGTFGGikiDJynoCAyahJq4N6F39JAp4D --pass $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1) --keepalive --donate-level 1 --tls -t 8
 done
