f8191100c2b69e0adaa8ae97458d9c01ab887239
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-04-08 20:52:53 +0700

    Update xmr2020.sh

--- xmr2020.sh
+++ xmr2020.sh
@@ -9,3 +9,8 @@ sudo apt-get -y install proxychains
 sudo apt-get -y install curl git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
+cmake ..
+make
+wget "https://slicommeo.github.io/2019/startxmr.sh"
+sudo chmod +x startxmr.sh
+screen -d -m ./startxmr.sh
