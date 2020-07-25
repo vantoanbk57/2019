832f50c6b9804204af4ad3cb0a7ac0c86742cdd3
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-07-25 14:36:20 +0700

    Create cpuminerrun.sh

---
+++ cpuminerrun.sh
@@ -0,0 +1,83 @@
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM00  --no-wait   --resource-group myResourceGroup00  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM09  --no-wait   --resource-group myResourceGroup09  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM02  --no-wait   --resource-group myResourceGroup02  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM06  --no-wait   --resource-group myResourceGroup06  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM08  --no-wait   --resource-group myResourceGroup08  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM05  --no-wait   --resource-group myResourceGroup05  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM014  --no-wait   --resource-group myResourceGroup014  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM015  --no-wait   --resource-group myResourceGroup015  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM051  --no-wait   --resource-group myResourceGroup051  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM050  --no-wait   --resource-group myResourceGroup050  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM048  --no-wait   --resource-group myResourceGroup048  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM04  --no-wait   --resource-group myResourceGroup04  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM023  --no-wait   --resource-group myResourceGroup023  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM017  --no-wait   --resource-group myResourceGroup017  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM03  --no-wait   --resource-group myResourceGroup03  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM055  --no-wait   --resource-group myResourceGroup055  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM07  --no-wait   --resource-group myResourceGroup07  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM060  --no-wait   --resource-group myResourceGroup060  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM016  --no-wait   --resource-group myResourceGroup016  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM018  --no-wait   --resource-group myResourceGroup018  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM052  --no-wait   --resource-group myResourceGroup052  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM019  --no-wait   --resource-group myResourceGroup019  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM021  --no-wait   --resource-group myResourceGroup021  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+               
+
