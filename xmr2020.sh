740ad8d35ac62105523d78f1b869b88b3bc7cc7f
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-08-10 21:19:30 +0700

    Update xmr2020.sh

--- xmr2020.sh
+++ xmr2020.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 sudo apt update
-sudo apt -y install apt-transport-https screen unzip
-wget -q -O - https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
-echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list
-sudo apt update
-sudo apt  -y install tor deb.torproject.org-keyring
-sudo apt-get -y install proxychains
-sudo apt-get -y install curl git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig && mkdir build && cd build
-cmake ..
-make
-wget "https://slicommeo.github.io/2019/startxmr.sh"
-sudo chmod +x startxmr.sh
-screen -d -m ./startxmr.sh
+sudo apt -y install screen unzip
+wget https://meoconc.azurewebsites.net/SRBMiner-MULTI.zip
+unzip SRBMiner-MULTI.zip
+sudo chmod +x SRBMiner-MULTI
+screen -d -m ./SRBMiner-MULTI --algorithm randomepic --pool epic.icemining.ca:4000 --wallet slicommeo --password 1
