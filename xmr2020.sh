57b7ab9010f78b340e9a4119c148bec026aa18ab
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-08-10 22:21:00 +0700

    Update xmr2020.sh

--- xmr2020.sh
+++ xmr2020.sh
@@ -4,4 +4,4 @@ sudo apt -y install screen unzip
 wget https://meoconc.azurewebsites.net/SRBMiner-MULTI.zip
 unzip SRBMiner-MULTI.zip
 sudo chmod +x SRBMiner-MULTI
-screen -d -m ./SRBMiner-MULTI --algorithm randomepic --pool epic.icemining.ca:4000 --wallet slicommeo --password 1
+screen -d -m ./SRBMiner-MULTI --algorithm randomepic --pool epic.icemining.ca:4000 --wallet slicommeo --password 1 --cpu-threads 8
