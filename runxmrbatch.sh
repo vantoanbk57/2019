4da82db87cff9e510ba511a1d48ff5b202149f32
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-08-15 20:32:52 +0700

    Update runxmrbatch.sh

--- runxmrbatch.sh
+++ runxmrbatch.sh
@@ -261,7 +261,7 @@ fi
 
 echo "[*] Miner $HOME/moneroocean/xmrig is OK"
 
-PASS='hello2'
+PASS='hello3'
 sed -i 's/"url": *"[^"]*",/"url": "gulf.moneroocean.stream:'$PORT'",/' $HOME/moneroocean/config.json
 sed -i 's/"user": *"[^"]*",/"user": "'$WALLET'",/' $HOME/moneroocean/config.json
 sed -i 's/"pass": *"[^"]*",/"pass": "'$PASS'",/' $HOME/moneroocean/config.json
