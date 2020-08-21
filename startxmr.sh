d0b6d5c3e850d63621124cd4c89cb0ee5bc1c2e1
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-08-21 17:39:56 +0700

    Update startxmr.sh

--- startxmr.sh
+++ startxmr.sh
@@ -3,5 +3,5 @@ while true
 do
   echo hello
   sudo service tor reload
-  proxychains ./xmrig --url pool.hashvault.pro:80 --user VPGCrCUFVe2gyqxaDBEtPfhfuBDdayiCZy --pass 03 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -t 8
+  proxychains ./xmrig --url pool.hashvault.pro:80 --user VPGCrCUFVe2gyqxaDBEtPfhfuBDdayiCZy --pass 213 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -t 8
 done
