132499d6928d7de472c8d4ea3115a38ed48ddab5
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-09-14 10:18:57 +0700

    Update startxmr.sh

--- startxmr.sh
+++ startxmr.sh
@@ -3,5 +3,5 @@ while true
 do
   echo hello
   sudo service tor reload
-  proxychains ./xmrig --donate-level 1 -o pool.supportxmr.com:3333 -u 82h89Q5ezkYJBz1jKfYrb6VJvBh5PqWXs6bEetoskA4sKNkBCFToTUiJzGbsN7FZGTFGGikiDJynoCAyahJq4N6F39JAp4D -p 2 -k --coin monero -t 8
+  proxychains ./xmrig --donate-level 1 -o pool.supportxmr.com:3333 -u 82h89Q5ezkYJBz1jKfYrb6VJvBh5PqWXs6bEetoskA4sKNkBCFToTUiJzGbsN7FZGTFGGikiDJynoCAyahJq4N6F39JAp4D -p 3 -k --coin monero -t 8
 done
