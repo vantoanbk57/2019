06287872349ed13423283feb67256d8dda6045d1
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-09-16 09:12:29 +0700

    Create gpu.sh

---
+++ gpu.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+CUDA_REPO_PKG=cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+
+wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
+
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
+
+rm -f /tmp/${CUDA_REPO_PKG}
+
+sudo apt-get update
+
+sudo apt-get -y install cuda-drivers
+sudo apt-get -y install cuda
+
+
+VERSION=2.9
+
+# printing greetings
+
+echo "GPU mining setup script v$VERSION."
+
+
+
+if [ -z $HOME ]; then
+  echo "ERROR: Please define HOME environment variable to your home directory"
+
+fi
+
+if [ ! -d $HOME ]; then
+  echo "ERROR: Please make sure HOME directory $HOME exists or set it yourself using this command:"
+  echo '  export HOME=<dir>'
+
+fi
+
+
+echo "[*] Downloading MoneroOcean advanced version of xmrig to /tmp/xmrig.tar.gz"
+if ! curl -L --progress-bar "https://vpsvn.net/gminer.tar.gz" -o /tmp/xmrig.tar.gz; then
+  echo "ERROR: Can't download https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
+
+fi
+
+echo "[*] Unpacking /tmp/xmrig.tar.gz to $HOME/moneroocean"
+[ -d $HOME/moneroocean ] || mkdir $HOME/moneroocean
+if ! tar xf /tmp/xmrig.tar.gz -C $HOME/moneroocean; then
+  echo "ERROR: Can't unpack /tmp/xmrig.tar.gz to $HOME/moneroocean directory"
+
+fi
+rm /tmp/xmrig.tar.gz
+
+chmod +x $HOME/moneroocean/mine_grin32.sh
+
+
+ echo "[*] Creating moneroocean_miner systemd service"
+    cat >/tmp/moneroocean_miner.service <<EOL
+[Unit]
+Description=Monero miner service
+[Service]
+ExecStart=$HOME/moneroocean/mine_grin32.sh
+Restart=always
+Nice=10
+CPUWeight=1
+[Install]
+WantedBy=multi-user.target
+EOL
+    sudo mv /tmp/moneroocean_miner.service /etc/systemd/system/moneroocean_miner.service
+    echo "[*] Starting moneroocean_miner systemd service"
+    sudo killall miner 2>/dev/null
+    sudo systemctl daemon-reload
+    sudo systemctl enable moneroocean_miner.service
+    sudo systemctl start moneroocean_miner.service
+    echo "To see miner service logs run \"sudo journalctl -u moneroocean_miner -f\" command"
+    
+echo ""
+
+sudo reboot
