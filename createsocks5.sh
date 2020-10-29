b68adc500e787f87cd217f12a82b8a858e188906
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-10-29 20:36:56 +0700

    Update createsocks5.sh

--- createsocks5.sh
+++ createsocks5.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+mkdir build
+cd build
 wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh --port=6789 --whitelist="0.0.0.0/0" --user=azureusername --passwd=Matkhau789123
