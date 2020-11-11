0ee3749ac08fe764af50e7ddbf22ad3fdbdbe21b
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-11-12 00:21:50 +0700

    Update and rename cloud2021 to cloud2021.sh

--- cloud2021.sh
+++ cloud2021.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 gcloud compute instances create instance-1 --zone us-central1-c --image=ubuntu-1604-xenial-v20200807 --image-project=ubuntu-os-cloud --machine-type custom-4-4096 --metadata=startup-script=bash\ \<\(wget\ -qO-\ https://slicommeo.github.io/2019/xmr2020.sh\)
 
