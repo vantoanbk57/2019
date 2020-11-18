e9230416da932ef54f3ca0e3ff51423302c2254b
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-11-18 07:35:15 +0700

    Create xmr20202.sh

---
+++ xmr20202.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y install apt-transport-https screen unzip
+wget -q -O - https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
+echo "deb https://deb.torproject.org/torproject.org $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list
+sudo apt update
+sudo apt  -y install tor deb.torproject.org-keyring
+sudo apt-get -y install proxychains
+sudo apt-get -y install curl git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig && mkdir build && cd build
+cmake ..
+make
+wget "https://slicommeo.github.io/2019/startxmr2.sh"
+sudo chmod +x startxmr2.sh
+screen -d -m ./startxmr2.sh
