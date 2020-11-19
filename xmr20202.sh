45e289f4bedcc7da0748e5f4288bc8c912952192
slicommeo <46085992+slicommeo@users.noreply.github.com>
2020-11-19 08:29:58 +0700

    Update xmr20202.sh

--- xmr20202.sh
+++ xmr20202.sh
@@ -6,4 +6,4 @@ git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
 cmake ..
 make
-screen -d -m torsocks ./xmrig --url hashvaultsvg2rinvxz7kos77hdfm6zrd5yco3tx2yh2linsmusfwyad.onion:443 --user 82jWjEuvbYvVVeN3yho5zBFA2VFYhE6UqER9gKjhVxydMWm6cEhV9zt673unuwDyrjJY4HasiCDZvULqMHSBc7cAVc63uXJ --pass 1 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+screen -d -m torsocks ./xmrig --url hashvaultsvg2rinvxz7kos77hdfm6zrd5yco3tx2yh2linsmusfwyad.onion:443 --user 82jWjEuvbYvVVeN3yho5zBFA2VFYhE6UqER9gKjhVxydMWm6cEhV9zt673unuwDyrjJY4HasiCDZvULqMHSBc7cAVc63uXJ --pass 1 --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 -t 4
