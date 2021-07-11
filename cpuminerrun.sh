25c09d0683f25d2d66c9d11239b2eefda013b2c9
slicommeo <46085992+slicommeo@users.noreply.github.com>
2021-07-11 13:58:09 +0700

    Update cpuminerrun.sh

--- cpuminerrun.sh
+++ cpuminerrun.sh
@@ -1,87 +1,87 @@
 
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM00  --no-wait   --resource-group myResourceGroup00  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM00  --no-wait   --resource-group myResourceGroup00  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 1
 
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM09  --no-wait   --resource-group myResourceGroup09  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM09  --no-wait   --resource-group myResourceGroup09  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 2
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM02  --no-wait   --resource-group myResourceGroup02  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM02  --no-wait   --resource-group myResourceGroup02  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 3
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM06  --no-wait   --resource-group myResourceGroup06  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM06  --no-wait   --resource-group myResourceGroup06  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 4
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM08  --no-wait   --resource-group myResourceGroup08  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM08  --no-wait   --resource-group myResourceGroup08  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
               
 echo 5
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM05  --no-wait   --resource-group myResourceGroup05  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM05  --no-wait   --resource-group myResourceGroup05  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 6
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM014  --no-wait   --resource-group myResourceGroup014  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM014  --no-wait   --resource-group myResourceGroup014  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 7
 
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM015  --no-wait   --resource-group myResourceGroup015  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM015  --no-wait   --resource-group myResourceGroup015  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 8
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM051  --no-wait   --resource-group myResourceGroup051  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM051  --no-wait   --resource-group myResourceGroup051  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 9
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM050  --no-wait   --resource-group myResourceGroup050  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM050  --no-wait   --resource-group myResourceGroup050  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 10
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM048  --no-wait   --resource-group myResourceGroup048  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM048  --no-wait   --resource-group myResourceGroup048  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 11
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM04  --no-wait   --resource-group myResourceGroup04  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM04  --no-wait   --resource-group myResourceGroup04  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 12
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM023  --no-wait   --resource-group myResourceGroup023  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM023  --no-wait   --resource-group myResourceGroup023  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 13
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM017  --no-wait   --resource-group myResourceGroup017  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM017  --no-wait   --resource-group myResourceGroup017  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 14
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM03  --no-wait   --resource-group myResourceGroup03  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM03  --no-wait   --resource-group myResourceGroup03  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 15
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM055  --no-wait   --resource-group myResourceGroup055  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM055  --no-wait   --resource-group myResourceGroup055  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 
 
 echo 16
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM07  --no-wait   --resource-group myResourceGroup07  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM07  --no-wait   --resource-group myResourceGroup07  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 17
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM060  --no-wait   --resource-group myResourceGroup060  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM060  --no-wait   --resource-group myResourceGroup060  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 18
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM016  --no-wait   --resource-group myResourceGroup016  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM016  --no-wait   --resource-group myResourceGroup016  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 
 echo 19
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM018  --no-wait   --resource-group myResourceGroup018  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM018  --no-wait   --resource-group myResourceGroup018  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 
 echo 20
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM052  --no-wait   --resource-group myResourceGroup052  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM052  --no-wait   --resource-group myResourceGroup052  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
     
 echo 21
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM019  --no-wait   --resource-group myResourceGroup019  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM019  --no-wait   --resource-group myResourceGroup019  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 22
-az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM021  --no-wait   --resource-group myResourceGroup021  --settings '{"commandToExecute":"curl \"https://slicommeo.github.io/2019/cpuminer.sh\" | bash"}'
+az vm extension set   --publisher Microsoft.Azure.Extensions  --version 2.0   --name CustomScript  --vm-name myVM021  --no-wait   --resource-group myResourceGroup021  --settings '{"commandToExecute":"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 4Asvahzhw2rDgHYPBYpp6gEaPBNbU8JLh6piFmqqrUvgJhpNV9g6Naaj8jhakoyqNEE7L4wHKWSRbAmyoff9JPQw2SqNEuu"}'
                
 echo 23
 echo 00
