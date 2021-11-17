01bbf6d7be6a1044c6140dc464f96e491c8b4d38
vantoanbk57 <vantoanbk57@gmail.com>
2021-11-17 20:42:26 +0700

    Update cpuminer2.sh

--- cpuminer2.sh
+++ cpuminer2.sh
@@ -1,131 +1,41 @@
 #!/bin/bash
-
-az group create --name myResourceGroup00 --location eastus
-az vm create  --resource-group myResourceGroup00  --name myVM00 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-
-                
-az group create --name myResourceGroup09 --location centralus
-az vm create  --resource-group myResourceGroup09  --name myVM09 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-                
-az group create --name myResourceGroup02 --location southcentralus
-az vm create  --resource-group myResourceGroup02  --name myVM02 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
+wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+tar xf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+rm cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+mv cpuminer-gr-1.2.4.1-x86_64_linux ~/cpuminer
 
 
 
-                
-az group create --name myResourceGroup06 --location northeurope
-az vm create  --resource-group myResourceGroup06  --name myVM06 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
+sed -i 's/RQKcAZBtsSacMUiGNnbk3h3KJAN94tstvt/'RQ4eKpR1PERqMumJwrrGwsaZe3UrPFRDVt'/' $HOME/cpuminer/config.json
+
+
+cat << EOF > miner.service
+[Unit]
+Description=GhostRider Miner
+After=network.target
+[Service]
+Type=simple
+User=root
+Group=root
+WorkingDirectory=$HOME/cpuminer
+ExecStart=$HOME/cpuminer/cpuminer.sh
+ExecStop=/usr/bin/pkill cpuminer
+Restart=always
+RestartSec=5s
+[Install]
+WantedBy=multi-user.target
+EOF
 
 
 
-                
-az group create --name myResourceGroup08 --location westeurope
-az vm create  --resource-group myResourceGroup08  --name myVM08 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-                
-az group create --name myResourceGroup05 --location southeastasia
-az vm create  --resource-group myResourceGroup05  --name myVM05 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
+sudo cp miner.service /etc/systemd/system/miner.service
+rm miner.service
 
 
 
-
-                
-az group create --name myResourceGroup014 --location eastasia
-az vm create  --resource-group myResourceGroup014  --name myVM014 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-
-                
-az group create --name myResourceGroup015 --location japaneast
-az vm create  --resource-group myResourceGroup015  --name myVM015 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-                
-az group create --name myResourceGroup051 --location japanwest
-az vm create  --resource-group myResourceGroup051  --name myVM051 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-                
-az group create --name myResourceGroup04 --location australiaeast
-az vm create  --resource-group myResourceGroup04  --name myVM04 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-
-
-                
-az group create --name myResourceGroup050 --location australiasoutheast
-az vm create  --resource-group myResourceGroup050  --name myVM050 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-                
-az group create --name myResourceGroup048 --location australiacentral
-az vm create  --resource-group myResourceGroup048  --name myVM048 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-
-                
-az group create --name myResourceGroup023 --location brazilsouth
-az vm create  --resource-group myResourceGroup023  --name myVM023 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-                
-az group create --name myResourceGroup017 --location canadacentral
-az vm create  --resource-group myResourceGroup017  --name myVM017 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-                
-az group create --name myResourceGroup03 --location westus2
-az vm create  --resource-group myResourceGroup03  --name myVM03 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-
-
-                
-az group create --name myResourceGroup055 --location canadaeast
-az vm create  --resource-group myResourceGroup055  --name myVM055 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-                
-az group create --name myResourceGroup07 --location uksouth
-az vm create  --resource-group myResourceGroup07  --name myVM07 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-                
-az group create --name myResourceGroup060 --location ukwest
-az vm create  --resource-group myResourceGroup060  --name myVM060 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-                
-az group create --name myResourceGroup016 --location koreacentral
-az vm create  --resource-group myResourceGroup016  --name myVM016 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-
-                
-az group create --name myResourceGroup052 --location koreasouth
-az vm create  --resource-group myResourceGroup052  --name myVM052 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-                
-az group create --name myResourceGroup018 --location francecentral
-az vm create  --resource-group myResourceGroup018  --name myVM018 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-
-
-                
-az group create --name myResourceGroup019 --location germanywestcentral
-az vm create  --resource-group myResourceGroup019  --name myVM019 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-
-
-                
-az group create --name myResourceGroup021 --location switzerlandnorth
-az vm create  --resource-group myResourceGroup021  --name myVM021 --size Standard_F8  --image UbuntuLTS   --admin-username azureuser   --generate-ssh-keys   --no-wait
-
-                
-sleep 161
+sudo systemctl daemon-reload
+sudo systemctl enable miner
+sudo systemctl start miner
 
 
 
